ce4c9dfbf2556866cadc414a1a7b8b584278b486
朱泽宁 <zening.zhu@ucarinc.com>
2018-12-18 17:38:13 +0800

    实现词法分析器

--- src/Run.java
+++ src/Run.java
@@ -5,11 +5,9 @@ import java.util.List;
  * @version 1.0
  * @date 2018/12/18
  */
-public class run {
+public class Run {
     public static void main(String[] args){
         List<Integer> token = LexicalAnalyzer.analyzer("D:/test.txt");
-        for (Integer toke : token){
-            System.out.print(toke+" ");
-        }
+
     }
 }
